fb4bbaacc1b504af13a6c0cc2578db55e5edb638
stefan-zobel <stefan-zobel@users.noreply.github.com>
2024-03-17 14:38:23 +0100

    extract accuracy computation

--- src/main/java/math/ml/mlp/CrossEntropyLoss.java
+++ src/main/java/math/ml/mlp/CrossEntropyLoss.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2024 Stefan Zobel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package math.ml.mlp;
 
 import math.dl.Hellinger;
-import math.dl.MultiClassAccuracy;
 import net.jamu.matrix.Matrices;
 import net.jamu.matrix.MatrixF;
 
@@ -10,8 +24,6 @@ import net.jamu.matrix.MatrixF;
  * distributions are.
  */
 public class CrossEntropyLoss extends AbstractLoss {
-
-    private final MultiClassAccuracy accuracy = new MultiClassAccuracy();
 
     public CrossEntropyLoss() {
     }
@@ -31,7 +43,6 @@ public class CrossEntropyLoss extends AbstractLoss {
         return computeGradients(prediction, expected);
     }
 
-    // TODO: can we express this as a matrix operation?
     private MatrixF computeGradients(MatrixF pred, MatrixF expect) {
         MatrixF gradients = Matrices.createF(pred.numRows(), pred.numColumns());
         for (int i = 0, colEnd = pred.endCol(), rowEnd = pred.endRow(); i <= colEnd; ++i) {
@@ -45,7 +56,6 @@ public class CrossEntropyLoss extends AbstractLoss {
         return gradients;
     }
 
-    // TODO: can we express this as a matrix operation?
     /**
      * Computes 1 x batchSize row vector of cross-entropy losses for same-sized
      * matrices of expected values (expect) and predicted values (pred) expressed as
@@ -86,15 +96,8 @@ public class CrossEntropyLoss extends AbstractLoss {
 
     private void computeAccuracy(MatrixF pred, MatrixF expect) {
         if (accuracyCallback != null) {
-            int length = pred.numRows();
-            float[] a = pred.getArrayUnsafe();
-            float[] b = expect.getArrayUnsafe();
-            for (int off = 0; off < length * pred.numColumns(); off += length) {
-                accuracy.compareF(length, a, off, b);
-            }
+            accuracyCallback.accept(CategorialAccuracy.computeAccuracy(pred, expect));
         }
-        accuracyCallback.accept(accuracy.getAverage());
-        accuracy.reset();
     }
 
     private static float log(float x) {
